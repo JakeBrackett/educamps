773f9ea5b90f0bd7421f16c28c741117c9285c1e
Jacob Bracket <jacob.brackett@me.com>
2017-03-03 14:43:21 -0800

    added php script to add user info to db

--- regpage/processreg.php
+++ regpage/processreg.php
@@ -1,4 +1,50 @@
 <? php 
-     $_POST["fname"]
+    $servername = "localhost"; 
+    $username = "root";
+    $password = "sql*OVERmysql";
+    $dbname = "educamps";
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    // Get user data
+    $fname = mysqli_real_escape_string($conn, $_POST["fname"]);
+    $lname = mysqli_real_escape_string($conn, $_POST["lname"]);
+    $email = mysqli_real_escape_string($conn, $_POST["email"]);
+    $phonenum = mysqli_real_escape_string($conn, $_POST["phonenum"]);
+    $password = mysqli_real_escape_string($conn, $_POST["password"]);
+    
+    function createSalt(){
+        $text = md5(uniqid(rand(), TRUE));
+        return substr($text, 0, 3);
+    }
+    $salt = createSalt();
+    $password = crypt($password, $salt);
+    
 
+    #Generate a random key from /dev/urandom, source - https://stackoverflow.com/questions/637278/what-is-the-best-way-to-generate-a-random-key-within-php
+    function get_key($bit_length = 16){
+        $fp = @fopen('/dev/urandom','rb');
+        if ($fp !== FALSE) {
+            $key = substr(base64_encode(@fread($fp,($bit_length + 7) / 8)), 0, (($bit_length + 5) / 6)  - 2);
+            @fclose($fp);
+            return $key;
+        }
+        return null;
+    }
+    $uuid = getkey();
+    if(uuid == NULL)
+        die("failed to generate UUID");
+    
+    
+    $sql = "INSERT INTO account(uuid, fname, lname, email, phone, password) VALUES ('$uuid', '$fname', '$lname', '$email','$phonenum', '$password')";
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } 
+    else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
     ?>
